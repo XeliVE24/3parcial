94d604e65d33ecab34f40574aaf501ba3de66e9d
XeliVE24 <xeli.vargas@uteca.edu.mx>
2024-04-08 19:21:32 -0600

    matriz

--- libs/functions.py
+++ libs/functions.py
@@ -1,4 +1,19 @@
+from openpyxl import load_workbook
 from .classes import *
+
+
+def getMatriz(archivo):
+    wb = load_workbook(archivo)
+    hoja = wb['Hoja1']
+    filas = hoja.max_row
+    columnas = hoja.max_column
+
+    # Imprimir los datos de la hoja de cálculo
+    for row in range(1, filas + 1):
+        for col in range(1,columnas + 1):
+            cell_value = hoja.cell(row=row, column=col).value
+            print(cell_value, end='\t')  
+        print()  
 
 def printDicc(dicc):
     for i in dicc:
@@ -7,5 +22,30 @@ def printDicc(dicc):
             print(f'\tRel:{j},peso:{dicc[i][j]}')
         return 0 
     
+
+class grafo():
+    def __init__(self) -> None:
+        self.aristas = {}
+        pass
+    
+    def addVertice(self,vertice):
+        self.aristas[vertice]={}
+        pass
+    
+    
+    #                   a       b       5
+    def addArista(self,origen,destino,peso):
+        if origen not in self.aristas:
+            self.addVertice(origen)
+        if destino not in self.aristas:
+            self.addVertice(destino)
+            
+        # self.aristas[origen][destino] = peso
+        self.aristas[origen].update({destino:peso})
+
+
+    def __str__(self) -> str:
+        return printDicc(self.aristas)
+    pass
 
 
--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 
-import pandas as pd
 from libs import *
 
 print("\033[34m Xeli Vargas \033[0m")
 archivo=input("Cual es el nombre del archivo a procesar?")
 
-df=pd.read_excel(archivo)
+
 print("\033[34m Matriz Adyacente :\033[0m")
-print (df)
+getMatriz(archivo)
 
 
+"""
 
 grafoTest =grafo()
 origenG = 'B'
@@ -52,4 +52,4 @@ for i in llaves:
                 del(path[i][kiss[1]])
             pass
     
-print(path)
+print(path)"""
