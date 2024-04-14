c969653a854737d214661c7807da7c7ddb4875db
XeliVE24 <xeli.vargas@uteca.edu.mx>
2024-04-14 17:54:35 -0600

    :)

--- libs/functions.py
+++ libs/functions.py
@@ -1,6 +1,5 @@
 from openpyxl import load_workbook
-from .classes import *
-
+import pandas as pd 
 
 def getMatriz(archivo):
     wb = load_workbook(archivo)
@@ -8,50 +7,47 @@ def getMatriz(archivo):
     filas = hoja.max_row
     columnas = hoja.max_column
 
-    dicc_relaciones = {}  # Diccionario para almacenar las relaciones
-
-    for row in range(2, filas + 1):  # Empezamos desde la segunda fila ya que la primera fila contiene los nombres de los nodos
+    dicc_relaciones = {}
+    for row in range(2, filas + 1):
         nodo = hoja.cell(row=row, column=1).value
-        dicc_relaciones[nodo] = {}  
-        for col in range(2, columnas + 1):  # Empezamos desde la segunda columna ya que la primera columna contiene los nombres de los nodos
+        dicc_relaciones[nodo] = {}
+        for col in range(2, columnas + 1):
             peso = hoja.cell(row=row, column=col).value
-            relacion = hoja.cell(row=1, column=col).value  
-            if relacion is not None and peso != 0:  #  peso es distinto de cero
+            relacion = hoja.cell(row=1, column=col).value
+            if relacion is not None and peso != 0:
                 dicc_relaciones[nodo][relacion] = peso
 
     return dicc_relaciones
 
-def printDicc(dicc):
-    for i in dicc:
-        print(f'nodo: {i}')
-        for j in dicc[i]:
-            print(f'\tRel:{j},peso:{dicc[i][j]}')
-        return 0 
-    
 
-class grafo():
-    def __init__(self) -> None:
-        self.aristas = {}
-        pass
+class Grafo:
+    def __init__(self, archivo) -> None:
+        self.Aristas = self.Excel(archivo)
     
-    def addVertice(self,vertice):
-        self.aristas[vertice]={}
-        pass
+    def Excel(self, archivo):
+        df = pd.read_excel(archivo, index_col=0)
+        return df.to_dict(orient='index')
     
+    def addVertice(self, vertice):
+        if vertice not in self.Aristas:
+            self.Aristas[vertice] = {}
     
-    #                   a       b       5
-    def addArista(self,origen,destino,peso):
-        if origen not in self.aristas:
-            self.addVertice(origen)
-        if destino not in self.aristas:
-            self.addVertice(destino)
-            
-        # self.aristas[origen][destino] = peso
-        self.aristas[origen].update({destino:peso})
-
-
+    def addArista(self, origen, destino, peso):
+        self.addVertice(origen)
+        self.addVertice(destino)
+        self.Aristas[origen][destino] = peso
+    
     def __str__(self) -> str:
-        return printDicc(self.aristas)
-    pass
+        return self.printDicc(self.Aristas)
+
+    def printDicc(self, dicc):
+        result = ""
+        for i in dicc:
+            result += f'nodo: {i}\n'
+            for j in dicc[i]:
+                result += f'\tRel:{j},peso:{dicc[i][j]}\n'
+        return result
+
+
 
 
--- main.py
+++ main.py
@@ -2,14 +2,14 @@
 from libs import *
 
 print("\033[34m Xeli Vargas \033[0m")
-
 archivo = input("¿Cuál es el nombre del archivo a procesar? ")
+
 
 # archivo Excel
 print("\033[34m Contenido del Archivo Excel :\033[0m")
 wb = load_workbook(archivo)
-for sheet in wb.sheetnames:
-    hoja = wb[sheet]
+for hoja in wb.sheetnames:
+    hoja = wb[hoja]
     for row in hoja.iter_rows(values_only=True):
         print("\t".join(str(cell) for cell in row))
 
@@ -17,51 +17,47 @@ for sheet in wb.sheetnames:
 print("\n\033[34m lista relaciones :\033[0m")
 matriz_adyacente = getMatriz(archivo)
 for nodo, relaciones in matriz_adyacente.items():
-    relaciones_str = ", ".join([f"[{relacion}] : {peso} ->" for relacion, peso in relaciones.items()])  # Crear una cadena de relaciones del nodo
+    relaciones_str = " ".join([f"[{nodo}] : {peso} ->" for relacion, peso in relaciones.items()]) 
     print(f"Nodo {nodo} -> {relaciones_str}")
 
 
-"""
+grafoTest = Grafo(archivo)
+print("")
+print(grafoTest)
 
-grafoTest =grafo()
 origenG = 'B'
 destinoG = 'H'
-path ={}
-visitados=[]
+path = {}
+visitados = []
 
 visitados.append(origenG)
-path[origenG] = {'-':0}
-llaves = grafoTest.aristas[origenG].keys()
-print(llaves)
+path[origenG] = {'-': 0}
+
+llaves = grafoTest.Aristas[origenG].keys()
 
 for i in llaves:
-    path[i]={origenG: grafoTest.aristas[origenG][i]}
+    path[i] = {origenG: grafoTest.Aristas[origenG][i]}
 
-print("primer iter")
-print(path)
 print(visitados)
 
-
-
 verticeAct = 'B'
-llaves = grafoTest.aristas[origenG].keys()
-print(llaves)
-
+visitados.append(verticeAct)
+llaves = grafoTest.Aristas[verticeAct].keys()
+print(f"\033[34m visitados\033[0m  {llaves}")
 for i in llaves:
     if i not in visitados:
-     #   self.aristas[origen].update((destino.peso))
         if i not in path: path[i]={}
-        llave=list(path[verticeAct].keys())
-        acumulado=path[verticeAct][llave[0]]
-        path[i].update({verticeAct: grafoTest.aristas[verticeAct][i]+acumulado})
+        llave = list(path[verticeAct].keys())
+        acumulado = path[verticeAct][llave[0]]
+        path[i].update({verticeAct : grafoTest.Aristas[verticeAct][i]+acumulado})
 
-#revisa si hay mas de dos llaves en una llave del path
-        if len(path[i]==2):
-            kiss=list(path[i].keys())
-            if kiss[0]>kiss[1]:
-                del (path[i][kiss[0]])
-            elif kiss[0]< kiss[1]:
+        if len(path[i]) == 2:
+            kiss = list(path[i].keys())
+            if kiss[0] > kiss[1]:
+                del(path[i][kiss[0]])
+            elif kiss[0] < kiss[1]:
                 del(path[i][kiss[1]])
             pass
-    
-print(path)"""
+
+print(f"\033[34m path: \033[0m  {path}")
+print("\033[34m cola:[#falta ]\033[0m")
